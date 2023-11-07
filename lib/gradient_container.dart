51f7a19a7ccd83ba9819524b33554174e9984272
MarcosH911 <marcosh.craft@gmail.com>
2023-11-07 19:59:25 +0100

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import "package:first_app/dice_roller.dart";
+import "package:flutter/material.dart";
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.startColor, this.endColor, {super.key});
+
+  final Color startColor, endColor;
+
+  void rollDice() {}
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            startColor,
+            endColor,
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: DiceRoller()),
+    );
+  }
+}
